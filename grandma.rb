2ed6627fa5b236d565a25a19b288d89abdbb9ade
Ebo Lee <yiboli.coding@gmail.com>
2020-07-08 19:23:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
   if phrase.upcase
       puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
+      puts "I LOVE YOU TOO PUMPKIN!"
   else 
       return "NO, NOT SINCE 1938!"
     end 
